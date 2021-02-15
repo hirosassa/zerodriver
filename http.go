68d5483b8a1d44171682a50709065ba8c288f501
hirosassa <hiro.sassa@gmail.com>
2021-02-15 22:26:50 +0900

    fix latency format

--- http.go
+++ http.go
@@ -2,10 +2,34 @@ package zerodriver
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/blendle/zapdriver"
 	"github.com/rs/zerolog"
 )
+
+type HTTPPayload struct {
+	RequestMethod                  string  `json:"requestMethod"`
+	RequestURL                     string  `json:"requestUrl"`
+	RequestSize                    string  `json:"requestSize"`
+	Status                         int     `json:"status"`
+	ResponseSize                   string  `json:"responseSize"`
+	UserAgent                      string  `json:"userAgent"`
+	RemoteIP                       string  `json:"remoteIp"`
+	ServerIP                       string  `json:"serverIp"`
+	Referer                        string  `json:"referer"`
+	Latency                        Latency `json:"latency"`
+	CacheLookup                    bool    `json:"cacheLookup"`
+	CacheHit                       bool    `json:"cacheHit"`
+	CacheValidatedWithOriginServer bool    `json:"cacheValidatedWithOriginServer"`
+	CacheFillBytes                 string  `json:"cacheFillBytes"`
+	Protocol                       string  `json:"protocol"`
+}
+
+type Latency struct {
+	Nanos   int32 `json:"nanos"`
+	Seconds int64 `json:"seconds"`
+}
 
 func (e *Event) HTTP(req *zapdriver.HTTPPayload) *zerolog.Event {
 	return e.Event.Interface("httpRequest", req)
@@ -13,4 +37,14 @@ func (e *Event) HTTP(req *zapdriver.HTTPPayload) *zerolog.Event {
 
 func NewHTTP(req *http.Request, res *http.Response) *zapdriver.HTTPPayload {
 	return zapdriver.NewHTTP(req, res)
+}
+
+func MakeLatency(d time.Duration) Latency {
+	nanos := d.Nanoseconds()
+	secs := nanos / 1e9
+	nanos -= secs * 1e9
+	return Latency{
+		Nanos:   int32(nanos),
+		Seconds: secs,
+	}
 }
--- http_test.go
+++ http_test.go
@@ -2,6 +2,7 @@ package zerodriver_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/blendle/zapdriver"
 	"github.com/hirosassa/zerodriver"
@@ -16,4 +17,32 @@ func TestHTTP(t *testing.T) {
 	event := e.HTTP(req)
 
 	assert.Equal(t, e.Interface("httpreqest", req), event)
+}
+
+func TestMakeLatency(t *testing.T) {
+	t.Parallel()
+
+	var tests = map[string]struct {
+		d    time.Duration
+		want zerodriver.Latency
+	}{
+		"zero": {
+			time.Duration(0),
+			zerodriver.Latency{Nanos: 0, Seconds: 0},
+		},
+		"10 sec": {
+			time.Duration(10 * 1e9),
+			zerodriver.Latency{Nanos: 0, Seconds: 10},
+		},
+		"sec and nano": {
+			time.Duration(123 + 456 * 1e9),
+			zerodriver.Latency{Nanos: 123, Seconds: 456},
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, tt.want, zerodriver.MakeLatency(tt.d))
+		})
+	}
 }
