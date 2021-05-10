10774453bd6cea727071c46571df3ca14de3b9ec
hirosassa <hiro.sassa@gmail.com>
2021-05-11 08:24:46 +0900

    test coverage (#15)
    * test coverage
    
    * fmt
    
    * update go mod
    
    * add coverage on CI
    
    * fix ci
    
    * fix ci
    
    * trigger ci
    
    * fix ci
    
    * add coverage badge on README

--- logger_test.go
+++ logger_test.go
@@ -1,54 +1,67 @@
-package zerodriver_test
+package zerodriver
 
 import (
+	"bytes"
 	"errors"
+	"os"
 	"testing"
 
-	"github.com/hirosassa/zerodriver"
+	"bou.ke/monkey"
+	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNew(t *testing.T) {
-	logger := zerodriver.NewProductionLogger()
-	assert.IsType(t, &zerodriver.Logger{}, logger)
+func TestNewProduction(t *testing.T) {
+	logger := NewProductionLogger()
+	assert.IsType(t, &Logger{}, logger)
+}
+
+func TestNewDevelopment(t *testing.T) {
+	logger := NewDevelopmentLogger()
+	assert.IsType(t, &Logger{}, logger)
 }
 
 func TestLoggers(t *testing.T) {
 	t.Parallel()
 
-	log := zerodriver.NewProductionLogger()
+	log := NewProductionLogger()
 
 	var tests = map[string]struct {
-		res  *zerodriver.Event
-		want *zerodriver.Event
+		res     *Event
+		want    *Event
+		wantErr error
 	}{
 		"trace": {
 			res:  log.Trace(),
-			want: &zerodriver.Event{log.Logger.Trace()},
+			want: &Event{log.Logger.Trace()},
 		},
 		"debug": {
 			res:  log.Debug(),
-			want: &zerodriver.Event{log.Logger.Debug()},
+			want: &Event{log.Logger.Debug()},
 		},
 		"info": {
 			res:  log.Info(),
-			want: &zerodriver.Event{log.Logger.Info()},
+			want: &Event{log.Logger.Info()},
 		},
 		"warn": {
 			res:  log.Warn(),
-			want: &zerodriver.Event{log.Logger.Warn()},
+			want: &Event{log.Logger.Warn()},
 		},
 		"error": {
 			res:  log.Error(),
-			want: &zerodriver.Event{log.Logger.Error()},
+			want: &Event{log.Logger.Error()},
 		},
 		"err": {
 			res:  log.Err(errors.New("some error")),
-			want: &zerodriver.Event{log.Logger.Err(errors.New("some error"))},
+			want: &Event{log.Logger.Err(errors.New("some error"))},
+		},
+		"with level": {
+			res:  log.WithLevel(zerolog.InfoLevel),
+			want: &Event{log.Logger.WithLevel(zerolog.InfoLevel)},
 		},
 		"log": {
 			res:  log.Log(),
-			want: &zerodriver.Event{log.Logger.Log()},
+			want: &Event{log.Logger.Log()},
 		},
 	}
 	for name, tt := range tests {
@@ -56,4 +69,98 @@ func TestLoggers(t *testing.T) {
 			assert.Equal(t, tt.want, tt.res)
 		})
 	}
+}
+
+func TestFatal(t *testing.T) {
+	t.Parallel()
+
+	// patch os.Exit
+	fakeExit := func(int) {
+		panic("os.Exit called")
+	}
+	patch := monkey.Patch(os.Exit, fakeExit)
+	defer patch.Unpatch()
+
+	// replace writer
+	log := NewProductionLogger()
+	out := &bytes.Buffer{}
+	logger := zerolog.New(out).With().Logger()
+	log.Logger = &logger
+
+	defer func() {
+		_ = recover()
+		actual := out.String()
+		out.Reset()
+
+		log.WithLevel(zerolog.FatalLevel).Msg("fatal")
+		expected := out.String()
+		assert.Equal(t, expected, actual)
+	}()
+
+	log.Fatal().Msg("fatal")
+}
+
+func TestPanic(t *testing.T) {
+	t.Parallel()
+
+	// replace writer
+	log := NewProductionLogger()
+	out := &bytes.Buffer{}
+	logger := zerolog.New(out).With().Logger()
+	log.Logger = &logger
+
+	defer func() {
+		_ = recover()
+		actual := out.String()
+		out.Reset()
+
+		log.WithLevel(zerolog.FatalLevel).Msg("panic")
+		expected := out.String()
+		assert.Equal(t, expected, actual)
+	}()
+
+	log.Panic().Msg("panic")
+}
+
+func TestPrint(t *testing.T) {
+	// replace writer
+	log := NewDevelopmentLogger()
+	out := &bytes.Buffer{}
+	logger := zerolog.New(out).With().Logger()
+	log.Logger = &logger
+
+	log.Print("print")
+	actual := out.String()
+	out.Reset()
+
+	log.Debug().Msg("print")
+	expected := out.String()
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestPrintf(t *testing.T) {
+	// replace writer
+	log := NewDevelopmentLogger()
+	out := &bytes.Buffer{}
+	logger := zerolog.New(out).With().Logger()
+	log.Logger = &logger
+
+	log.Printf("print: %s", "hello")
+	actual := out.String()
+	out.Reset()
+
+	log.Debug().Msgf("print: %s", "hello")
+	expected := out.String()
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestWrite(t *testing.T) {
+	t.Parallel()
+
+	log := NewProductionLogger()
+	n, err := log.Write([]byte("abc"))
+	assert.Equal(t, 3, n)
+	assert.NoError(t, err)
 }
